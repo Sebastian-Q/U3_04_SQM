63f26ca75ef197d0e207f697e95dd5592d799b7a
Sebastian-Q <20203tn049@utez.edu.mx>
2025-06-16 17:36:53 -0600

    [FIRST COMMIT] Creación del repositorio y subida de archivos

---
+++ src/main/java/utez/edu/mx/u3_04_sqm/U304SqmApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.u3_04_sqm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U304SqmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U304SqmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/u3_04_sqm/U304SqmApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.u3_04_sqm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U304SqmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
